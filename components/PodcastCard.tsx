b1df438a7885f994e0ddc185a42db688e38aeaa1
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-02 12:58:41 +0700

    setup convex

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -15,7 +15,13 @@ export default function PodcastCard({
   return (
     <div className="cursor-pointer">
       <figure className="flex flex-col gap-2">
-        <Image src={imgUrl} alt={title} width={174} height={174} />
+        <Image
+          src={imgUrl}
+          alt={title}
+          width={174}
+          height={174}
+          className="aspect-square h-fit w-full rounded-xl 2xl:size-[200px ]"
+        />
         <div className="flex flex-col">
           <h1 className="text-16 truncate font-bold text-white-1">{title}</h1>
           <h2 className="text-12 truncate font-normal capitalize text-white-4">
