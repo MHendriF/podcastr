5c6c77381fc0887cbfe9f1410e229cd54e5ab6fb
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-02 16:08:09 +0700

    create ui form create podcast

---
+++ components/GeneratePodcast.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function GeneratePodcast() {
+  return <div>GeneratePodcast</div>;
+}
---
+++ components/GenerateThumbnail.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function GenerateThumbnail() {
+  return <div>GenerateThumbnail</div>;
+}
