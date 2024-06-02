c67e73348a00246665b3d62703d6e8dcc6d8c349
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-02 15:19:38 +0700

    implement authentication with clerk and convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://fine-pony-55.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
