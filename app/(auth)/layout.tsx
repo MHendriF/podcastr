23f182d3cbe55429637991bc80d1ef1a32182eaa
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-02 15:09:55 +0700

    implement ui sign-in and sign-up, update middleware

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,21 @@
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <main>{children}</main>
-    </div>
+    <main className="relative h-screen w-full">
+      <div className="absolute size-full">
+        <Image
+          src="/images/bg-img.png"
+          alt="background"
+          fill
+          className="size-full"
+        />
+        {children}
+      </div>
+    </main>
   );
 }
---
+++ app/providers/ConvexClerkProvider.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
+
+const convex = new ConvexReactClient(
+  process.env.NEXT_PUBLIC_CONVEX_URL as string
+);
+
+const ConvexClerkProvider = ({ children }: { children: React.ReactNode }) => (
+  <ClerkProvider
+    publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}
+    appearance={{
+      layout: {
+        socialButtonsVariant: "iconButton",
+        logoImageUrl: "/icons/auth-logo.svg",
+      },
+      variables: {
+        colorBackground: "#15171c",
+        colorPrimary: "",
+        colorText: "#fff",
+        colorInputBackground: "#1b1f29",
+        colorInputText: "#fff",
+      },
+    }}
+  >
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+      {children}
+    </ConvexProviderWithClerk>
+  </ClerkProvider>
+);
+
+export default ConvexClerkProvider;
