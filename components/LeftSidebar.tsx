a83881b70310f73586eb3c69c7b4d821256e1516
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-02 13:50:25 +0700

    integration convex to app

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { sidebarLinks } from "@/constants/indext";
+import { sidebarLinks } from "@/constants/index";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
